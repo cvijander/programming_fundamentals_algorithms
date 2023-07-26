0dfd61ae284cb1eede3749fc8554b6dd51ae2535
cvijander <cvija85@gmail.com>
2023-07-27 01:35:59 +0200

    stack structures

---
+++ data_structures_stack.py
@@ -0,0 +1,23 @@
+# try out the Python stack functions
+
+
+#todo create a new empty stack
+stack = []
+
+# todo push items onto the stack
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+
+#todo print the stack contents
+print(stack)
+
+
+#todo pop an item off the stack 
+x= stack.pop()
+print(x)
+print(stack)
+
