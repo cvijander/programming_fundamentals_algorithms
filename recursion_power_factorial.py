2fddd4c2e61adc862012affe7c11fc9920d81435
cvijander <cvija85@gmail.com>
2023-07-27 18:14:13 +0200

    power and factorial

---
+++ recursion_power_factorial.py
@@ -0,0 +1,25 @@
+# using recursion to implement power and factorial functions
+
+# 2 na 4   -> 2 * 2 * 2 * 2
+def power(num, pwr):
+    if pwr ==0:
+        return 1
+    else:
+        return num * power(num,pwr-1)
+
+
+# 5!  -> 5 * 4 * 3 * 2 * 1 
+def factorial(num):
+    if num == 0:
+      return 1
+    else:
+        return num *factorial(num - 1)
+
+
+
+print("{} to the power of {} is {}".format(5,3,power(5,3)))
+print("{} to the power of {} is {}".format(1,5,power(1,5)))
+
+print("{}! is {} ".format(4,factorial(4)))
+print("{}! is {}".format(0,factorial(0)))
+
